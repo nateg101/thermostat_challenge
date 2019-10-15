7949cbb7e33c40519a0aeb63a321069da5b8d0c3
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-15 14:25:55 +0100

    Added down and Min Temp functions.

--- jasmine-standalone-3.5.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/ThermostatSpec.js
@@ -12,7 +12,21 @@ describe('Thermostat', function(){
   });
 
   it("can increase the temperature with an 'up' function", function(){
-    thermostat.up(5)
-    expect(thermostat.getTemp()).toEqual(25)
+    thermostat.up()
+    expect(thermostat.getTemp()).toEqual(21)
+  });
+
+  it("can decrease the temperature with a 'down' function", function(){
+    thermostat.down()
+    expect(thermostat.getTemp()).toEqual(19)
+  });
+
+  describe("Temperature limits",function(){
+    it("stops the temperature going below 10",function(){
+      for (var i = 0; i < 11; i++){
+      thermostat.down();
+      }
+      expect(thermostat.getTemp()).toEqual(10)
+    })
   });
 });
--- jasmine-standalone-3.5.0/src/Thermostat.js
+++ jasmine-standalone-3.5.0/src/Thermostat.js
@@ -2,11 +2,23 @@
 
 function Thermostat(){
   this.temperature = 20
+  this.MIN_TEMP = 10
 }
 
 Thermostat.prototype.getTemp = function(){
   return this.temperature;
 }
-Thermostat.prototype.up = function(number){
-  this.temperature += number;
+Thermostat.prototype.up = function(){
+  this.temperature += 1;
+}
+
+Thermostat.prototype.down = function(){
+  if (this.isMIN_TEMP()){
+    return;
+  }
+  this.temperature -= 1;
+}
+
+Thermostat.prototype.isMIN_TEMP = function(){
+  return this.temperature === this.MIN_TEMP;
 }
