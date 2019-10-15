836bae99698e9d32496f7b65d6685410b9db6be8
AlecDWalker <walkeralec@hotmail.com>
2019-10-15 14:53:17 +0100

    power saver mode set up

--- jasmine-standalone-3.5.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/ThermostatSpec.js
@@ -21,12 +21,40 @@ describe('Thermostat', function(){
     expect(thermostat.getTemp()).toEqual(19)
   });
 
+  describe("power saver mode", function(){
+    it("starts with power saver mode on", function(){
+      expect(thermostat.isPowerSaver()).toBe(true)
+    });
+    it("can switch power saver mode off", function(){
+      thermostat.powerSaverOff();
+      expect(thermostat.isPowerSaver()).toBe(false)
+    });
+    it("can switch power saver mode on", function(){
+      thermostat.powerSaverOff();
+      thermostat.powerSaverOn();
+      expect(thermostat.isPowerSaver()).toBe(true)
+    });
+  });
+
   describe("Temperature limits",function(){
     it("stops the temperature going below 10",function(){
       for (var i = 0; i < 11; i++){
       thermostat.down();
       }
       expect(thermostat.getTemp()).toEqual(10)
-    })
+    });
+    it("has a max temperature of 32 with the power saver off", function(){
+      for (var i = 0; i < 15; i++){
+      thermostat.up();
+      }
+      expect(thermostat.getTemp()).toEqual(32)
+    });
+    it("has a max temperature of 25 with the power saver on", function(){
+      thermostat.saverOn
+      for (var i = 0; i < 6; i++){
+      thermostat.up();
+      }
+      expect(thermostat.getTemp()).toEqual(25)
+    });
   });
 });
--- jasmine-standalone-3.5.0/src/Thermostat.js
+++ jasmine-standalone-3.5.0/src/Thermostat.js
@@ -3,12 +3,30 @@
 function Thermostat(){
   this.temperature = 20
   this.MIN_TEMP = 10
+  this.MAX_TEMP = 32
+  this.powerSaver = true
+}
+
+Thermostat.prototype.isPowerSaver = function(){
+  return this.powerSaver;
+}
+
+Thermostat.prototype.powerSaverOff = function(){
+  this.powerSaver = false;
+}
+
+Thermostat.prototype.powerSaverOn = function(){
+  this.powerSaver = true;
 }
 
 Thermostat.prototype.getTemp = function(){
   return this.temperature;
 }
+
 Thermostat.prototype.up = function(){
+  if (this.isMAX_TEMP()){
+    return;
+  }
   this.temperature += 1;
 }
 
@@ -21,4 +39,8 @@ Thermostat.prototype.down = function(){
 
 Thermostat.prototype.isMIN_TEMP = function(){
   return this.temperature === this.MIN_TEMP;
+}
+
+Thermostat.prototype.isMAX_TEMP = function(){
+  return this.temperature === this.MAX_TEMP;
 }
